d47401f1ac6654b6b61cd11166743790637cd9c7
RodrigoSoaresNascimento <rodrigosnascimento1@gmail.com>
2022-08-21 00:21:31 -0300

    Adicionando os endPoints de financeiro

--- src/main/java/br/com/dbc/vemser/dbcompras/service/CompraService.java
+++ src/main/java/br/com/dbc/vemser/dbcompras/service/CompraService.java
@@ -175,23 +175,35 @@ public class CompraService {
         return compraServiceUtil.converterCompraEntityToCompraDTO(compraRepository.save(compra));
     }
 
-//    public List<CompraWithValorItensDTO> list(Integer idCompra) {
-//
-//        if (idCompra == null) {
-//
-//            return compraRepository.findAll()
-//                    .stream()
-//                    .map(compraServiceUtil::converterCompraEntityToCompraWithValor)
-//                    .toList();
-//        } else {
-//
-//            return compraRepository.findById(idCompra)
-//                    .stream()
-//                    .map(compraServiceUtil::converterCompraEntityToCompraWithValor)
-//                    .toList();
-//        }
-//
-//    }
+    public List<CompraWithValorItensDTO> list(Integer idCompra) {
+
+        if (idCompra == null) {
+
+           return compraRepository.findAll()
+                   .stream()
+                   .map(compraEntity -> {
+                       try {
+                           return compraServiceUtil.converterCompraEntityToCompraWithValor(compraEntity);
+                       } catch (RegraDeNegocioException e) {
+                           throw new RuntimeException(e);
+                       }
+                   })
+                   .toList();
+        } else {
+
+            return compraRepository.findById(idCompra)
+                    .stream()
+                    .map(compraEntity -> {
+                        try {
+                            return compraServiceUtil.converterCompraEntityToCompraWithValor(compraEntity);
+                        } catch (RegraDeNegocioException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }).toList();
+
+        }
+
+    }
 
 //    public List<CompraWithValorItensDTO> listFinanceiro(String nomeUsuario, String nomeCompra) {
 ////        return compraRepository.listByNomeUsuario(nomeUsuario, nomeCompra)
